c0d63db2b4729fe279627aac905baea57bc1bd00
anderss0n <daneshmand.ali@hotmail.com>
2017-07-07 19:28:12 +0430

    Added Product relationship to the Category model.

--- app/Category.php
+++ app/Category.php
@@ -1,10 +1,16 @@
 <?php
 
-namespace anetwork;
+namespace AliAssignment;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    /*
+     * Relationship with AliAssignment\Product.
+     */
+    public function product()
+    {
+        return $this->hasMany('AliAssignment\Product', 'fk_category');
+    }
 }
