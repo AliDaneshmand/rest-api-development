6c5280c9f7cd273afe1bbcac817f852215a31cd0
anderss0n <daneshmand.ali@hotmail.com>
2017-07-07 21:22:51 +0430

    Limit on Category title length and fix propduct seeder

--- database/migrations/2017_07_06_101109_category.php
+++ database/migrations/2017_07_06_101109_category.php
@@ -15,7 +15,7 @@ class Category extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('title')->unique();
+            $table->string('title', 30)->unique();
             $table->integer('priority');
         });
     }
